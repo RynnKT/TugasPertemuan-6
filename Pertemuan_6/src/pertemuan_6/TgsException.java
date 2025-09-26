15e762541a2d3179e22e815c1d7df4f7bba96eaa
RynnKT <ontab100@gmail.com>
2025-09-26 11:08:58 +0700

    Add files via upload

---
+++ Pertemuan_6/src/pertemuan_6/TgsException.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan_6;
+
+/**
+ *
+ * @author Ridho
+ */
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public class TgsException extends Exception {
+        public TgsException(String pesan) {
+            super(pesan);
+        }
+    }
